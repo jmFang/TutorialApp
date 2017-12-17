5095bab42e92d6fe0f742400363026bce1fb5575
jmFang <fangjiamou.com@outlook.com>
2017-12-17 23:12:58 +0800

    initial project

---
+++ app/src/main/java/com/example/jiamoufang/tutorialapp/ui/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.jiamoufang.tutorialapp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jiamoufang.tutorialapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
